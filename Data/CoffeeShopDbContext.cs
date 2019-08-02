9d75cb56032823621e09acddb94ba4933e47a2cd
Alex Greene <alex@roofsnap.com>
2019-08-01 23:06:21 -0400

    initial commit

---
+++ Data/CoffeeShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeShopApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShopApp.Data
+{
+    public class CoffeeShopDbContext : DbContext
+    {
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Reservation> Type { get; set; }
+    }
+}
